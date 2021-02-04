dfd37853d39e0c6c34bf5c2ab204914581d7c53c
ktakahashi24 <k.taka4114@gmail.com>
2021-02-05 08:39:29 +0900

    文化人の方へのDMそうふ

--- src/send_dm.rb
+++ src/send_dm.rb
@@ -1,9 +1,8 @@
 require 'dotenv'
-
 require_relative 'client.rb'
 
 # 送信する対象を切り替える場合は、ここのmoduleを変更する
-require_relative 'dm_messages/kohei_hirate/0127.rb'
+require_relative 'dm_messages/0129.rb'
 
 class SendDm
   include Client
@@ -11,12 +10,21 @@ class SendDm
 
   def execute
     error_users = []
+
     target_user_ids.each do |user_id|
-      user = client.user(user_id)
-      twitter_id = user.id
-      client.create_direct_message(twitter_id, text_message, {})
-      rescue Twitter::Error::Forbidden
+      begin
+        user = client.user(user_id)
+        twitter_id = user.id
+
+        # とりあえず１分まつ（連続でcallすると制限に引っかかって制限食らうので）
+        sleep(61)
+
+        client.create_direct_message(twitter_id, text_message, {})
+        p "[#{user_id}] - sent!!!"
+      rescue StandardError => e
+        p "[#{user_id}] - #{e.message}"
         error_users.push user_id
+      end
     end
 
     unless error_users.empty?
